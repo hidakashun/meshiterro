77ad65612fdc9f11dc8800fdd71abb042c0ccecd
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-14 10:32:09 +0000

    [finish]4-11

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,12 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user#PostImage モデルに関連付けられるのは、1 つの User モデルなので単数形の「user」にする。
 
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
 end
