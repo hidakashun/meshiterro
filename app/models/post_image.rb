a0f1e33840e2ef3dbd7c67a3a24d308f43a77fe5
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-14 04:43:16 +0000

    [finish]4-9

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,6 @@
 class PostImage < ApplicationRecord
 
   has_one_attached :image
+  belongs_to :user#PostImage モデルに関連付けられるのは、1 つの User モデルなので単数形の「user」にする。
 
 end
--- db/migrate/20230813093855_create_post_images.rb
+++ db/migrate/20230813093855_create_post_images.rb
@@ -1,7 +1,7 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-    　#t.integer :id(投稿画像を識別する IDは必要だが、定義すると重複してしまうため入れない)
+    　#t.integer :id(投稿画像を識別する IDは必要だが、テーブルを作成した際に自動的に付与されるので記述しない)
       t.string :shop_name
       t.text :caption
       t.integer :user_id
