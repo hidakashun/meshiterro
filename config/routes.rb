6e1cc4be9f19861c32d2e07e28d07b026bffc17b
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-15 12:33:04 +0000

    [finish]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: :about
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]#単数形にすると、/:idがURLに含まれなくなります
     resources :post_comments, only: [:create, :destroy]
+
   end
 
   resources :users, only: [:show, :edit, :update]
